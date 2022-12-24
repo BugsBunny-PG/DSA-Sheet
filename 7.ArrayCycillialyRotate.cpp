c48ac4499186ebf42ca9839a9b1e8da2c63f7a5a
Pragya Gupta <106425223+BugsBunny-PG@users.noreply.github.com>
2022-12-24 23:42:06 +0530

    Update 7.ArrayCycillialyRotate.cpp

--- 7.ArrayCycillialyRotate.cpp
+++ 7.ArrayCycillialyRotate.cpp
@@ -1,13 +1,25 @@
 //https://practice.geeksforgeeks.org/problems/cyclically-rotate-an-array-by-one/0
 #include<bits/stdc++.h>
 using namespace std;
-void rotate(int arr[], int n)
+//bruteforce technique
+
+/*void rotate(int arr[], int n)
 {
   int temp=arr[n-1];
   for(int i=n-1;i>0;i--){
   	arr[i]=arr[i-1];
   }  
   arr[0]=temp;
+}*/
+
+//two pointer method 
+void rotate(int arr[],int n){
+	int i=0,j=n-1;
+	while(i!=j){
+		swap(arr[i],arr[j]);
+		 i++;
+	}
+	
 }
 int main(){
 	int n;
